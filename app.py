7a8d31d4766ad2dd5613b2c6033678045715550a
KI1208 <keisuke1208@gmail.com>
2021-06-01 08:25:51 +0900

    For new site

--- app.py
+++ app.py
@@ -12,10 +12,14 @@ username = sys.argv[1]
 password = sys.argv[2]
 company = sys.argv[3]
 
-USERNAME_XPATH = '/html/body/div/table/tbody/tr/td/form/table[2]/tbody/tr/td[1]/table[1]/tbody/tr/td/table[1]/tbody/tr[4]/td[3]/input'
-PASSWORD_XPATH = '/html/body/div/table/tbody/tr/td/form/table[2]/tbody/tr/td[1]/table[1]/tbody/tr/td/table[1]/tbody/tr[6]/td[3]/input'
-COMPANY_XPATH = '/html/body/div/table/tbody/tr/td/form/table[2]/tbody/tr/td[1]/table[1]/tbody/tr/td/table[1]/tbody/tr[2]/td[3]/input'
-LOGIN_XPATH = '/html/body/div/table/tbody/tr/td/form/table[2]/tbody/tr/td[1]/table[1]/tbody/tr/td/table[2]/tbody/tr[2]/td[2]/a/img'
+USERNAME_XPATH = '//*[@id="userId"]'
+PASSWORD_XPATH = '//*[@id="password"]'
+COMPANY_XPATH = '//*[@id="companyId"]'
+LOGIN_XPATH = '//*[@id="loginBtn"]'
+MONTHLY_APPROVAL = '//*[@id="js-sort-task"]/li[2]/div/span/a'
+ALL_CHECK = '//*[@id="paging"]/div[4]/ul/li[1]/label/span[1]'
+APPROVAL = '//*[@id="approval-btn-top"]/a'
+APPROVAL_ALL = '//*[@id="close-approval-btn-tooltip"]'
 
 SLEEP_TIME = 5
 
@@ -30,7 +34,7 @@ driver = webdriver.Chrome(service_log_path=os.path.devnull,
                           executable_path='chromedriver.exe',)
                           # options=chrome_options)
 # 対象ページを読み込ませる。まずはログインページ。
-driver.get("https://timecard2.e-staffing.ne.jp/000_002.cfm")
+driver.get("https://portal.e-staffing.ne.jp/client/mnu/creload/ja")
 wait = ui.WebDriverWait(driver, 10)
 wait.until(page_is_loaded)
 
@@ -50,49 +54,29 @@ driver.find_element_by_xpath(LOGIN_XPATH).click()
 wait = ui.WebDriverWait(driver, 10)
 wait.until(page_is_loaded)
 
-# HTML解析
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'html.parser')
-tables = soup.findChildren('table', class_='cmTableDetail cmWidthP90 cmCenter')
-# tables[0]
-tbody = tables[0].find_all('tbody', recursive=False)
-rows = tbody[0].find_all('tr', recursive=False)
-# メンバー数は、len(rows) - 1
+# 月次承認
+time.sleep(SLEEP_TIME)
+driver.find_element_by_xpath(MONTHLY_APPROVAL).click()
+wait = ui.WebDriverWait(driver, 10)
+wait.until(page_is_loaded)
 
-for i in range(len(rows)-1):
-    # 承認ページへ。承認したものからなくなるため、最初のXPATHは固定
-    MEMBER_XPATH = '/html/body/div[1]/div/form[1]/table[4]/tbody/tr[2]/td[9]/input'
-    driver.find_element_by_xpath(MEMBER_XPATH).click()
-    wait = ui.WebDriverWait(driver, 10)
-    wait.until(page_is_loaded)
+# 承認
+driver.switch_to.window(driver.window_handles[1])
+time.sleep(SLEEP_TIME)
+driver.find_element_by_xpath(ALL_CHECK).click()
+driver.find_element_by_xpath(APPROVAL).click()
+wait = ui.WebDriverWait(driver, 10)
+wait.until(page_is_loaded)
 
-    # メンバーページ内HTML解析
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    tables = soup.findChildren('table', class_='cmTableDetail cmWidthP90')
-    # tables[0]
-    tbody = tables[0].find_all('tbody', recursive=False)
-    rows = tbody[0].find_all('tr', recursive=False)
+# 各自承認
+time.sleep(SLEEP_TIME)
+for i in range(20):
+    CHECKBOX = '//*[@id="closestatus"]/div[1]/div[2]/div[' + str(i + 1) + ']/div[1]/span/label/span[1]'
+    try:
+        driver.find_element_by_xpath(CHECKBOX).click()
+    except:
+        break
 
-    for j in range(2, len(rows)+1):
-        APPROVE_XAPTH = '/html/body/div[1]/table[8]/tbody/tr[' + str(j) + ']/td[13]/table/tbody/tr/td[1]/input'
-        try:
-            driver.find_element_by_xpath(APPROVE_XAPTH).click()
-            # 承認するとポップアップが発生
-            alert_obj = driver.switch_to.alert
-            time.sleep(SLEEP_TIME)
-            alert_obj.accept()
-            time.sleep(SLEEP_TIME)
-        except:
-            print('Line ' + str(j) + ' failed.')
+if driver.find_element_by_xpath(APPROVAL_ALL).is_enabled():
+    driver.find_element_by_xpath(APPROVAL_ALL).click()
 
-    APPROVE_CLOSE_XPATH = '/html/body/div[1]/form[2]/table[1]/tbody/tr[2]/td[9]/table/tbody/tr/td[1]/input'
-    driver.find_element_by_xpath(APPROVE_CLOSE_XPATH).click()
-    time.sleep(SLEEP_TIME)
-    alert_obj.accept()
-    time.sleep(SLEEP_TIME)
-
-    RETURN_XPATH = '/html/body/div[1]/form[5]/table/tbody/tr/th/input'
-    driver.find_element_by_xpath(RETURN_XPATH).click()
-    wait = ui.WebDriverWait(driver, 10)
-    wait.until(page_is_loaded)
